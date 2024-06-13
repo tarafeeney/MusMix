6553ce3c936d1b914cb23098deab2b717d866f34
BoundlessFate <villac3@rpi.edu>
2024-06-13 13:00:13 -0400

    added page routing

--- webapp/src/app/song-input/song-input.component.ts
+++ webapp/src/app/song-input/song-input.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-song-input',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './song-input.component.html',
+  styleUrl: './song-input.component.css'
 })
-export class AppComponent {
-  title = 'MusMix';
+export class SongInputComponent {
+
 }
