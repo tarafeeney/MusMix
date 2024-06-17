cf86af88e1ab3c626e4c5c1532755e215148cf76
noobzhuz <75611029+noobzhuz@users.noreply.github.com>
2024-06-17 11:35:15 -0400

    Update testAccess.py

--- db/testAccess.py
+++ db/testAccess.py
@@ -1,4 +1,13 @@
 import pymongo
+from bson.objectid import ObjectId
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["MusMix"]
+collection = db.users
+result = collection.update_one(
+    {"_id": ObjectId("666b067c17df24017f6ecf5a")},
+    {"$set": {"name": "Alice", "age": 30}}
+)
+
+
+
