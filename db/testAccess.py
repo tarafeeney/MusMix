532ac73877945937094f981f5e4dcd969c9c1510
noobzhuz <75611029+noobzhuz@users.noreply.github.com>
2024-06-24 12:17:32 -0400

    create a function to create or use a created connection

--- db/testAccess.py
+++ db/testAccess.py
@@ -1,6 +1,22 @@
+from flask import Flask,g
 import pymongo
 from sha import sha256
+import dbConnection
 
+app = Flask(__name__)
+
+client = dbConnection.connect_mongo()
+db = client["musmixdata"]
+collection = db.userdata
+username = "testuser"
+password = "111111"
+result = collection.find_one({"auth": sha256.generate_hash(username + password).hex()})
+if result == None:
+    print('username or password incorrect')
+else:
+    print(result['type'])
+
+'''
 client = pymongo.MongoClient("mongodb+srv://MusMixAdmin1:uGpw6A07tp2kbTP7@musmix.bb1xivw.mongodb.net/",
                              username = "MusMixAdmin1", password = "uGpw6A07tp2kbTP7")
 db = client["musmixdata"]
@@ -13,5 +29,4 @@ if result == None:
     print('username or password incorrect')
 else:
     print(result['type'])
-
-
+'''
