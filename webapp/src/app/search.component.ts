74633711d066229cc1cc08f1fdb1a5c83f870b40
AnnabelZhang1 <annabel.zhang1@gmail.com>
2024-06-24 11:05:38 -0400

    made search bar that listens for enter key input

--- webapp/src/app/search.component.ts
+++ webapp/src/app/search.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -8,7 +9,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './searchPage.html',
   styleUrl: './search.css'
 })
+
+// adding in variables
 export class SearchComponent {
   title = 'Search Page';
   username = "NEW USER"
 }
+
+console.log("hi");
+window.addEventListener('DOMContentLoaded', (event) => {
+  const searchInput = document.getElementById('input') as HTMLInputElement;
+
+  searchInput.addEventListener('keydown', (e: KeyboardEvent) => {
+      if (e.key === 'Enter') {
+          alert('You have submitted a song with query: ' + searchInput.value);
+      }
+  });
+});
+
