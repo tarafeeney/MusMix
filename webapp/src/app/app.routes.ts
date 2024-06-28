37a1d0865c2b486e5a83a1f4a146d082c7e9992e
BoundlessFate <villac3@rpi.edu>
2024-06-28 16:18:22 -0400

    fixed some weird issues while merging

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { SearchComponent } from './search.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/mainPage', pathMatch: 'full'},
     { path: 'mainPage', component: AppComponent },
+    { path: 'login', component: LoginComponent },
     { path: 'searchPage', component: SearchComponent}
 ];
