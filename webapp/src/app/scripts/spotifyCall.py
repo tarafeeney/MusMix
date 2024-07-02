de9468eacac84acc53585d8a9d1b86315f254b47
BoundlessFate <villac3@rpi.edu>
2024-07-01 22:41:18 -0400

    updated spotifyCall

--- webapp/src/app/scripts/spotifyCall.py
+++ webapp/src/app/scripts/spotifyCall.py
@@ -12,12 +12,45 @@ authManager = SpotifyClientCredentials(client_id=clientId, client_secret=clientS
 sp = spotipy.Spotify(auth_manager=authManager)
 
 results = sp.search(q=f"track:{songName} artist:{artist}", type='track')
-# Extract the first track (assuming it's the most relevant match)
 track = results['tracks']['items'][0]
+genres = sp.artist(sp.search(q=artist, type='artist', limit=1)['artists']['items'][0]['id'])['genres']
+features = sp.audio_features(track['id'])[0]
+# Extract the first track (assuming it's the most relevant match)
 
 # Print some basic information about the track
 print(f"Track Name: {track['name']}")
 print(f"Artist: {track['artists'][0]['name']}")
-print(f"Album: {track['album']['name']}")
-print(f"Release Date: {track['album']['release_date']}")
-print(f"Popularity: {track['popularity']}")
+print(f"Genres: {genres}")
+print("-----FEATURES-----")
+print(f"BPM: {features['tempo']}")
+print(f"Key: {features['key']}")
+print(f"Mode: {features['mode']}")
+print(f"Acousticness: {features['acousticness']}")
+print(f"Danceability: {features['danceability']}")
+print(f"Energy: {features['energy']}")
+print(f"Speechiness: {features['speechiness']}")
+print(f"Valence: {features['valence']}")
+print("-----NOW SEARCHING FOR OTHER SONGS-----")
+bpmTolerance=30
+for genre in genres:
+    # Gather 50 songs in the same genre
+    allSongs = sp.search(q=f"genre:{genre}", type="track", limit=50)
+    for song in allSongs['tracks']['items']:
+            # Song is good if it hit this point
+    #    print(f"Track Name: {song['name']}")
+    #   print(f"Artist: {song['artists'][0]['name']}")
+        # Skip if song is what was searched
+        if song['id'] == track['id']:
+            continue
+        curFeatures = sp.audio_features(song['id'])[0]
+        if curFeatures['key'] != features['key']:
+            continue
+        if curFeatures['mode'] != features['mode']:
+            continue
+        if curFeatures['tempo'] - bpmTolerance > features['tempo']:
+            continue
+        if curFeatures['tempo'] + bpmTolerance < features['tempo']:
+            continue
+        # Song is good if it hit this point
+        print(f"Track Name: {song['name']}")
+        print(f"Artist: {song['artists'][0]['name']}")
